0ea08fcbc35b04f5cdae7815dd998aa69282140a
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-06 09:46:17 +0300

    Add exercise <Reverse a String>

---
+++ CSharp-Advanced/Unit03_lab/1. Reverse a String.cs
@@ -0,0 +1,16 @@
+﻿namespace _1._Reverse_a_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Stack<char> chars = new Stack<char>();
+            string input = Console.ReadLine();
+            foreach (char ch in input)
+            {
+                chars.Push(ch);
+            }
+            Console.WriteLine(string.Join("", chars.ToArray()));
+        }
+    }
+}
