0d67ea137c6ed0348b82b14dc67fdd19f5a77fb4
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-07 10:44:38 +0300

    Add exercises Stack Sum

---
+++ 2. Stack Sum.cs
@@ -0,0 +1,45 @@
+﻿namespace _2._Stack_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+               
+            
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>(input);
+
+            string command = Console.ReadLine();
+
+            while (command.ToUpper() != "END")
+            {
+                string[] commandSplits = command.Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                if (commandSplits[0].ToUpper() == "ADD")
+                {
+                    stack.Push(int.Parse(commandSplits[1]));
+                    stack.Push(int.Parse(commandSplits[2]));
+                }
+                else if (commandSplits[0].ToUpper() == "REMOVE")
+                {
+                    int countToRemove = int.Parse(commandSplits[1]);    
+
+                    if (stack.Count >= countToRemove)
+                    {
+                        for (int i = 0; i < countToRemove; i++)
+                        {
+                            stack.Pop();
+                        }
+                    }
+                }
+                command = Console.ReadLine();
+            }
+
+            Console.WriteLine($"Sum: {stack.Sum()}");
+        }
+    }
+}
