0c99eec51a554419fb52f5155e02c155707de44b
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-08 11:34:50 +0300

    Add exercise Supermarket

---
+++ CSharp-Advanced/Unit03_lab/6. Supermarket.cs
@@ -0,0 +1,32 @@
+﻿namespace _6._Supermarket
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Queue<string> queue = new Queue<string>();
+            
+            string input = Console.ReadLine();
+
+            while (input != "End")
+            {
+                if (input == "Paid")
+                {
+                    int queueCount = queue.Count;
+                    for (int i = 0; i < queueCount; i++)
+                    {
+                        Console.WriteLine(queue.Dequeue());
+                    }
+                }
+                else
+                {
+                    queue.Enqueue(input);
+                }
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{queue.Count} people remaining.");
+        }
+    }
+}
