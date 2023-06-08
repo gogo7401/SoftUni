0c6ce54492e1a5907dbc2f7926b0a7555ca0827d
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-08 11:37:50 +0300

    Add exercise Hot Potato

---
+++ CSharp-Advanced/Unit03_lab/7. Hot Potato.cs
@@ -0,0 +1,39 @@
+﻿namespace _7._Hot_Potato
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            int burnCount = int.Parse(Console.ReadLine());
+
+            Queue<string> kids = new Queue<string>(input);
+
+            int currentCount = 1;
+
+            string currendKid = null;
+
+            while (kids.Count > 1)
+            {
+                if (currentCount == burnCount)
+                {
+                    Console.WriteLine($"Removed {kids.Dequeue()}");
+                    currentCount = 1;
+                }
+                else
+                {
+                    currendKid = kids.Dequeue();
+                    kids.Enqueue(currendKid);
+                    currentCount++;
+                }
+
+
+
+            }
+
+            Console.WriteLine($"Last is {kids.Dequeue()}");
+        }
+    }
+}
