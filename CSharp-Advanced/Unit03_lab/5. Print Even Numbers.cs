01c380696c831672407c3bd7d7454aaf62754d5d
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-08 11:33:48 +0300

    Add exercise Print Even Numbers

---
+++ CSharp-Advanced/Unit03_lab/5. Print Even Numbers.cs
@@ -0,0 +1,28 @@
+﻿namespace _5._Print_Even_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>(input);
+
+            int queueCount = queue.Count;
+
+            for (int i = 0; i < queueCount; i++)
+            {
+                int element = queue.Dequeue();
+                if (element % 2 == 0)
+                {
+                    queue.Enqueue(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", queue));
+        }
+    }
+}
