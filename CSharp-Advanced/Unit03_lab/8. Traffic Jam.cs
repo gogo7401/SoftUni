dc1a9d425ea94141461eaaad0c0d41b1b7543d39
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-08 11:38:59 +0300

    Add exercise Traffic Jam

---
+++ CSharp-Advanced/Unit03_lab/8. Traffic Jam.cs
@@ -0,0 +1,44 @@
+﻿using System.Diagnostics.Contracts;
+
+namespace _8._Traffic_Jam
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            string input = Console.ReadLine();
+
+            Queue<string> cars = new Queue<string>();
+
+            int passedCarsCount = 0;
+
+            while (input.ToLower() != "end")
+            {
+                if (input.ToLower() == "green")
+                {
+                    int carToPass = cars.Count;
+                    if (carToPass > n)
+                    {
+                        carToPass = n;
+                    }
+                    for (int i = 0; i < carToPass; i++)
+                    {
+                        Console.WriteLine($"{cars.Dequeue()} passed!");
+                        passedCarsCount++;
+                    }
+                }
+                else
+                {
+                    cars.Enqueue(input);
+                }
+
+
+                input = Console.ReadLine();
+            }
+
+            Console.WriteLine($"{passedCarsCount} cars passed the crossroads.");
+        }
+    }
+}
