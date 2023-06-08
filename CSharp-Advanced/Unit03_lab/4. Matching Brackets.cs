890825f6723785064502c5a154c8a46e67277faf
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-08 11:31:26 +0300

    Add exercise Matching Brackets

---
+++ CSharp-Advanced/Unit03_lab/4. Matching Brackets.cs
@@ -0,0 +1,30 @@
+﻿namespace _4._Matching_Brackets
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 + (2 - (2 + 3) * 4 / (3 + 1)) * 5
+
+            string input = Console.ReadLine();
+
+            Stack<int> brackets = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+
+                if (input[i] == '(')
+                {
+                    brackets.Push(i);
+                }
+                else if (input[i] == ')')
+                {
+                    int startIndex = brackets.Pop();
+                    Console.WriteLine(input.Substring(startIndex,i - startIndex + 1 ));
+                }
+
+
+            }
+        }
+    }
+}
