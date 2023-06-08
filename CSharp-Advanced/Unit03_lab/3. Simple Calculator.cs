aeb2b5958cd0ccb0d34cd496dd460e647f767acc
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-08 11:30:19 +0300

    Add exercise Simple Calculator

---
+++ CSharp-Advanced/Unit03_lab/3. Simple Calculator.cs
@@ -0,0 +1,37 @@
+﻿namespace _3._Simple_Calculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Reverse()
+                .ToArray();
+
+            Stack<string> stack = new Stack<string>(input);
+
+            while (stack.Count > 1)
+            {
+                int firstoperand = int.Parse(stack.Pop());
+                string sign = stack.Pop();
+                int SecondOperand = int.Parse(stack.Pop());
+
+                int result = 0;
+
+                if (sign == "-")
+                {
+                    result = firstoperand - SecondOperand;
+                    stack.Push(result.ToString());
+                }
+                else if (sign == "+")
+                {
+                    result = firstoperand + SecondOperand;
+                    stack.Push(result.ToString());
+                }
+            }
+
+            Console.WriteLine(stack.Pop());
+        }
+    }
+}
