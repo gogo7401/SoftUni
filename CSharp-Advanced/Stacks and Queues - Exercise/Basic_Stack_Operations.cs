e2613b475981bd0d28c6ddb2d2a550e5ff67803d
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-12 11:06:31 +0300

    Add exercise Basic_Stack_Operations

---
+++ CSharp-Advanced/Stacks and Queues - Exercise/Basic_Stack_Operations.cs
@@ -0,0 +1,49 @@
+﻿namespace _01._Basic_Stack_Operations
+{
+    internal class Basic_Stack_Operations
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            int[] elements = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Stack<int> stack = new Stack<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                stack.Push(elements[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                stack.Pop();
+            }
+
+            if (stack.Count == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            if (stack.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(stack.Min());
+            }
+        }
+    }
+}
