1c1ca7deb8d78862a8855967ae58caa9000b3d84
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-12 11:03:16 +0300

    Add exercise Basic Queue operations

---
+++ CSharp-Advanced/Stacks and Queues - Exercise/Basic_Queue_Operations.cs
@@ -0,0 +1,49 @@
+﻿namespace _02._Basic_Queue_Operations
+{
+    internal class Basic_Queue_Operations
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int n = input[0];
+            int s = input[1];
+            int x = input[2];
+
+            int[] elements = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> queue = new Queue<int>();
+
+            for (int i = 0; i < n; i++)
+            {
+                queue.Enqueue(elements[i]);
+            }
+
+            for (int i = 0; i < s; i++)
+            {
+                queue.Dequeue();
+            }
+
+            if (queue.Count == 0)
+            {
+                Console.WriteLine(0);
+                return;
+            }
+
+            if (queue.Contains(x))
+            {
+                Console.WriteLine("true");
+            }
+            else
+            {
+                Console.WriteLine(queue.Min());
+            }
+        }
+    }
+}
