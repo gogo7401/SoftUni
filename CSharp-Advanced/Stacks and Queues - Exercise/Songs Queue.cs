8476bbb694d4a0ccbb1b90428a3ee9646b1b390b
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-13 08:41:25 +0300

    Add exercise Songs Queue

---
+++ CSharp-Advanced/Stacks and Queues - Exercise/Songs Queue.cs
@@ -0,0 +1,47 @@
+﻿namespace _06._Songs_Queue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries);
+
+            Queue<string> queueSongs = new Queue<string>(input);
+
+            string command = string.Empty;
+
+            while (queueSongs.Count > 0)
+            {
+                command = Console.ReadLine();
+
+                if (command == "Play" && queueSongs.Count > 0)
+                {
+                    queueSongs.Dequeue();
+                }
+
+                if (command == "Show")
+                {
+                    Console.WriteLine(string.Join(", ", queueSongs));
+                }
+
+                if (command.Substring(0,3) == "Add")
+                {
+                    string newSong = command.Substring(4);
+                    if (queueSongs.Contains(newSong))
+                    {
+                        Console.WriteLine($"{newSong} is already contained!");
+                    }
+                    else
+                    {
+                        queueSongs.Enqueue(newSong);
+                    }
+                }
+
+            }
+
+            Console.WriteLine("No more songs!");
+
+        }
+    }
+}
