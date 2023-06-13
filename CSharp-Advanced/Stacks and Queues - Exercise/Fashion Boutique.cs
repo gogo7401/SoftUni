ef4a196440f55184d71ed0a638b89438e460e7a3
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-13 08:40:41 +0300

    Add exercise Fashion Boutique

---
+++ CSharp-Advanced/Stacks and Queues - Exercise/Fashion Boutique.cs
@@ -0,0 +1,49 @@
+﻿namespace _05._Fashion_Boutique
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] clothesValue = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int rackValue = int.Parse(Console.ReadLine());
+
+            Stack<int> box = new Stack<int>(clothesValue);
+
+            int passedClothesValue = 0;
+            int curreenClothesValue = 0;
+            int rackCount = 1;
+
+            while (box.Count > 0)
+            {
+                curreenClothesValue = box.Peek();
+                if ((curreenClothesValue + passedClothesValue) > rackValue)
+                {
+                    rackCount++;
+                    passedClothesValue = 0;
+                }
+                else if ((curreenClothesValue + passedClothesValue) == rackValue)
+                {
+                    if (box.Count != 1)
+                    {
+                        rackCount++;
+                        passedClothesValue = 0;
+                    }
+                    box.Pop();
+
+                }
+                else if ((curreenClothesValue + passedClothesValue) < rackValue)
+                {
+                    passedClothesValue += curreenClothesValue;
+                    box.Pop();
+                }
+                
+            }
+
+            Console.WriteLine(rackCount);
+        }
+    }
+}
