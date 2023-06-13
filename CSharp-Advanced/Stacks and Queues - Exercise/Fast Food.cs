226bbabadb233ecb193630f7bb263e8ccd13885d
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-13 08:39:45 +0300

    Add exercise Fast Food

---
+++ CSharp-Advanced/Stacks and Queues - Exercise/Fast Food.cs
@@ -0,0 +1,49 @@
+﻿namespace _04._Fast_Food
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int totalQuantity = int.Parse(Console.ReadLine());
+
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Queue<int> orders = new Queue<int>(input);
+
+            Boolean isComplete = true;
+
+            if (orders.Count > 0)
+            {
+                Console.WriteLine(orders.Max());
+            }
+
+            int currentOrderQuantity = 0;
+
+            while (orders.Count > 0)
+            {
+                currentOrderQuantity = orders.Peek();
+
+                if (totalQuantity >= currentOrderQuantity)
+                {
+                    totalQuantity -= currentOrderQuantity;
+                    orders.Dequeue();
+                }
+                else
+                {
+                    Console.WriteLine($"Orders left: {string.Join(" ", orders)}");
+                    isComplete= false;
+                    break;
+                }
+
+            }
+
+            if (isComplete)
+            {
+                Console.WriteLine("Orders complete");
+            }
+        }
+    }
+}
