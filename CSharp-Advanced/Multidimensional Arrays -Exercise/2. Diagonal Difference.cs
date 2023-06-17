5f569d2e7feec99dccd396d81210e254a5d7c68e
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-17 11:11:16 +0300

    Multidimensional Arrays -Exercise

---
+++ CSharp-Advanced/Multidimensional Arrays -Exercise/2. Diagonal Difference.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Linq;
+
+namespace _2._Diagonal_Difference
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            FillMatrix(matrix);
+
+            int sumLeft = 0;
+            int sumRight = 0;
+            int count = 1;
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (row == col)
+                    {
+                        sumLeft += matrix[row, col];
+                    }
+                }
+            }
+
+            for (int row = 0; row < matrix.GetLength(0); row++)
+            {
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    if (col == (matrix.GetLength(1) - 1 - row))
+                    {
+                        sumRight += matrix[row, col];
+                    }
+                }
+            }
+
+            Console.WriteLine(Math.Abs(sumLeft-sumRight));
+
+
+        }
+
+        static void FillMatrix(int[,] matrix)
+        {
+            for (int row  = 0; row < matrix.GetLength(0); row++)
+            {
+                int[] input = Console.ReadLine()
+                         .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                         .Select(int.Parse)
+                         .ToArray();
+
+                for (int col = 0; col < matrix.GetLength(1); col++)
+                {
+                    matrix[row, col] = input[col];
+                }
+            }
+        }
+    }
+}
