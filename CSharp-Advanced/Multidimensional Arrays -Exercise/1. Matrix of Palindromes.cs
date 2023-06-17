68e13ad4cb61f3c3924d52d130c634d46e01bbcd
gogo7401 <120330216+gogo7401@users.noreply.github.com>
2023-06-17 11:10:47 +0300

    Multidimensional Arrays -Exercise

---
+++ CSharp-Advanced/Multidimensional Arrays -Exercise/1. Matrix of Palindromes.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Linq;
+using System.Text;
+
+namespace _1._Matrix_of_Palindromes
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] matrixSize = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            int matrixRows = matrixSize[0];
+            int matrixCols = matrixSize[1];
+
+            string[,] palindrom = new string[matrixRows, matrixCols];
+
+            char[] symbols = "abcdefghijklmnopqrstuvwxyz".ToCharArray();
+
+            for (int row = 0;  row < matrixRows; row++)
+            {
+                StringBuilder cell = new StringBuilder();
+
+                for (int i = 0; i < matrixCols; i++)
+                {
+                    cell.Append(symbols[row]);
+                    cell.Append(symbols[i+row]);
+                    cell.Append(symbols[row]);
+                    palindrom[row, i] = cell.ToString();
+                    cell.Clear();   
+                }
+
+
+            }
+
+            for (int row = 0; row < matrixRows; row++)
+            {
+                for (int col = 0; col < matrixCols; col++)
+                {
+                    Console.Write(palindrom[row, col] + " ");
+                }
+                Console.WriteLine(string.Empty);
+            }
+
+        }
+    }
+}
