cb29c6f9217a582a9d97bbbbea6188ae1c0879a5
Georgi Georgiev <joro74@gmail.com>
2023-06-25 17:30:43 +0300

    Add files via upload

---
+++ CSharp-Advanced/Sets and Dictionaries Advanced - Lab/01. Count Same Values in Array.cs
@@ -0,0 +1,34 @@
+﻿namespace _01._Count_Same_Values_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // input // -2.5 4 3 -2.5 -5.5 4 3 3 -2.5 3
+
+            double[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+
+            var dict = new Dictionary<double, int> ();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (dict.ContainsKey(input[i]) == false)
+                {
+                    dict.Add(input[i], 0);
+                }
+
+                dict[input[i]]++;
+            }
+
+            foreach (var (value, count) in dict)
+            {
+                Console.WriteLine($"{value} - {count} times");
+            }
+
+
+        }
+    }
+}
