065629379d7a087781e50c49a13ca6b71a27428f
Georgi Georgiev <joro74@gmail.com>
2023-06-27 08:39:42 +0300

    Add files via upload

---
+++ CSharp-Advanced/Sets and Dictionaries Advanced - Lab/05. Cities by Continent and Country.cs
@@ -0,0 +1,50 @@
+﻿namespace _05._Cities_by_Continent_and_Country
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            var continents = new Dictionary<string, Dictionary<string, List<string>>>();
+
+            for (int i = 0; i < count; i++)
+            {
+                string[] line = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                string continent = line[0];
+                string country = line[1];
+                string city = line[2];
+
+                if (continents.ContainsKey(continent) == false)
+                {
+                    continents.Add(continent, new Dictionary<string, List<string>>());
+                }
+
+                if (continents[continent].ContainsKey(country) == false)
+                {
+                    continents[continent].Add(country, new List<string>());
+                }
+
+                continents[continent][country].Add(city);   
+            }
+
+            //Europe:
+            //Bulgaria->Sofia, Plovdiv
+
+            foreach (var (continent, countryAndCities) in continents)
+            {
+                Console.WriteLine($"{continent}:");
+
+                foreach (var (country, cities) in countryAndCities)
+                {
+                    Console.WriteLine($"{country} -> " + string.Join(", ", cities));
+                }
+
+
+            }
+
+        }
+    }
+}
