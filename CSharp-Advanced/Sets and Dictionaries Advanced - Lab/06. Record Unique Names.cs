4f3854181c907edfc6f7c555d3ec79b876360f19
Georgi Georgiev <joro74@gmail.com>
2023-06-27 08:39:55 +0300

    Add files via upload

---
+++ CSharp-Advanced/Sets and Dictionaries Advanced - Lab/06. Record Unique Names.cs
@@ -0,0 +1,19 @@
+﻿namespace _06._Record_Unique_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            HashSet<string> names = new HashSet<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                names.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine(string.Join(Environment.NewLine, names));
+        }
+    }
+}
