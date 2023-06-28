04fd6a9b715dc971ea97fb174d4ec0dd2243f0d1
Georgi Georgiev <joro74@gmail.com>
2023-06-28 10:07:16 +0300

    Add files via upload

---
+++ CSharp-Advanced/07. Parking Lot.cs
@@ -0,0 +1,38 @@
+﻿namespace _07._Parking_Lot
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<string> parkingLot = new HashSet<string>();    
+            
+            string line;
+
+            while ((line = Console.ReadLine()) != "END")
+            {
+                string[] tokens = line.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+                string command = tokens[0];
+                string licenseplate = tokens[1];
+
+                if (command == "IN")
+                {
+                    parkingLot.Add(licenseplate);
+                }
+                else if (command == "OUT")
+                {
+                    parkingLot.Remove(licenseplate);
+                }
+            }
+
+            if (parkingLot.Count == 0)
+            {
+                Console.WriteLine("Parking Lot is Empty");
+            }
+            else
+            {
+
+            Console.WriteLine(string.Join(Environment.NewLine, parkingLot));
+            }
+        }
+    }
+}
