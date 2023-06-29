0369f6b61149792fa6bb822f9500b6d1dc256766
Georgi Georgiev <joro74@gmail.com>
2023-06-29 16:51:35 +0300

    Add files via upload

---
+++ CSharp-Advanced/Sets and Dictionaries Advanced - Exercise/02. Sets of Elements.cs
@@ -0,0 +1,37 @@
+﻿namespace _02._Sets_of_Elements
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] setsLenght = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray(); 
+
+            int setOne = setsLenght[0];
+            int setTwo = setsLenght[1];
+
+            HashSet<int> setsOne = new HashSet<int>();
+            HashSet<int> setsTwo = new HashSet<int>();
+
+            for (int i = 0; i < setOne;  i++)
+            {
+                setsOne.Add(int.Parse(Console.ReadLine()));
+            }
+
+            for (int i = 0; i < setTwo; i++)
+            {
+                setsTwo.Add(int.Parse(Console.ReadLine()));
+            }
+
+            foreach (var item in setsOne)
+            {
+                if (setsTwo.Contains(item))
+                {
+                    Console.Write($"{item} ");
+                }
+            }
+        }
+    }
+}
