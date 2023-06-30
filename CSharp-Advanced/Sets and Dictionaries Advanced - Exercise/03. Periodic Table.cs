52ca278cb33d2808a58406cfdae6794994790e20
Georgi Georgiev <joro74@gmail.com>
2023-06-30 07:29:46 +0300

    Add files via upload

---
+++ CSharp-Advanced/Sets and Dictionaries Advanced - Exercise/03. Periodic Table.cs
@@ -0,0 +1,26 @@
+﻿namespace _03._Periodic_Table
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lineCount = int.Parse(Console.ReadLine());
+
+            SortedSet<string>   periodicTable = new SortedSet<string>();
+
+            for (int i = 0; i < lineCount; i++)
+            {
+                string[] elements = Console.ReadLine()
+                    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+                foreach (string element in elements)
+                {
+                    periodicTable.Add(element);
+                }
+            }
+
+            Console.WriteLine(string.Join(" ", periodicTable));
+
+        }
+    }
+}
