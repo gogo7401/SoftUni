cfa1b8a898d2429660445465cc729373f9878b54
Georgi Georgiev <joro74@gmail.com>
2023-06-30 07:30:11 +0300

    Add files via upload

---
+++ CSharp-Advanced/Sets and Dictionaries Advanced - Exercise/04. Even Times.cs
@@ -0,0 +1,32 @@
+﻿namespace _04._Even_Times
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int count = int.Parse(Console.ReadLine());
+
+            var dict = new Dictionary<int, int>();  
+
+            for (int i = 0; i < count; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (dict.ContainsKey(number) == false)
+                {
+                    dict.Add(number, 0);
+                }
+
+                dict[number]++;
+            }
+
+            foreach (var (numberKey, numberValue) in dict)
+            {
+                if (numberValue % 2 == 0)
+                {
+                    Console.WriteLine(numberKey);
+                    break;
+                }
+            }
+        }
+    }
+}
