f5fce25f4cb5df6be3fee6083c64ec3a3bad5a09
Georgi Georgiev <joro74@gmail.com>
2023-07-04 17:10:18 +0300

    Add files via upload

---
+++ CSharp-Advanced/Sets and Dictionaries Advanced - Exercise/05. Count Symbols.cs
@@ -0,0 +1,26 @@
+﻿namespace _05._Count_Symbols
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] chars = Console.ReadLine().ToCharArray();
+
+            var sortedDict = new SortedDictionary<char, int>();
+
+            foreach (char c in chars)
+            {
+                if (sortedDict.ContainsKey(c) == false)
+                {
+                    sortedDict.Add(c, 0);
+                }
+                sortedDict[c]++;
+            }
+
+            foreach (var (ch, times) in sortedDict)
+            {
+                Console.WriteLine($"{ch}: {times} time/s");
+            }
+        }
+    }
+}
