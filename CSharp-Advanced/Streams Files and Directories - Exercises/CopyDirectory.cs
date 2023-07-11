83e945677bf8c54c61adc387f41c12785b0f990d
Georgi Georgiev <joro74@gmail.com>
2023-07-11 08:35:14 +0300

    Add files via upload

---
+++ CSharp-Advanced/Streams Files and Directories - Exercises/CopyDirectory.cs
@@ -0,0 +1,36 @@
+﻿namespace CopyDirectory
+{
+    using System;
+    using System.IO;
+
+    public class CopyDirectory
+    {
+        static void Main()
+        {
+            string inputPath =  @$"{Console.ReadLine()}";
+            string outputPath = @$"{Console.ReadLine()}";
+
+            CopyAllFiles(inputPath, outputPath);
+        }
+
+        public static void CopyAllFiles(string inputPath, string outputPath)
+        {
+            if (Directory.Exists(outputPath))
+            {
+                Directory.Delete(outputPath, true);
+                Directory.CreateDirectory(outputPath);
+            }
+            else
+            {
+                Directory.CreateDirectory(outputPath);
+            }
+            
+            foreach (var file in Directory.GetFiles(inputPath))
+            {
+                FileInfo fileInfo = new FileInfo(file);
+                string outputFile = outputPath + "/" + fileInfo.Name;
+                File.Copy(file, outputFile);
+            }
+        }
+    }
+}
