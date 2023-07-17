a4483810de9804ff59b4c29dbdbf215f24dfa7b7
Georgi Georgiev <joro74@gmail.com>
2023-07-17 10:53:22 +0300

    Add files via upload

---
+++ CSharp-Advanced/Functional Programming - Lab/04. Add VAT.cs
@@ -0,0 +1,15 @@
+﻿namespace _04._Add_VAT
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<double, string> addVAT = x => (x * 1.2D).ToString("F2");
+            double[] input = Console.ReadLine()
+                .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(double.Parse)
+                .ToArray();
+            Console.WriteLine(string.Join(Environment.NewLine, input.Select(x => addVAT(x))));
+        }
+    }
+}
