a508ee19de20e59036e512a74451ff73bf0f0ec1
Georgi Georgiev <joro74@gmail.com>
2023-07-17 10:53:10 +0300

    Add files via upload

---
+++ CSharp-Advanced/Functional Programming - Lab/03. Count Uppercase Words.cs
@@ -0,0 +1,18 @@
+﻿namespace _03._Count_Uppercase_Words
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // 1 variant
+            //Predicate<string> isCapitalLetter = x => x[0] == x.ToUpper()[0];
+            //2 variant
+            Predicate<string> isCapitalLetter = x => char.IsUpper(x[0]);
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            Console.WriteLine(string.Join(Environment.NewLine, input
+                .Where(x => isCapitalLetter(x))));
+        }
+    }
+}
