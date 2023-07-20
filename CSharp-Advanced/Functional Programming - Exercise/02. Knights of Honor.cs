70940e8a901938c3d99db48a7cec528b97aad58f
Georgi Georgiev <joro74@gmail.com>
2023-07-20 08:33:22 +0300

    Add files via upload

---
+++ CSharp-Advanced/Functional Programming - Exercise/02. Knights of Honor.cs
@@ -0,0 +1,18 @@
+﻿namespace _02._Knights_of_Honor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string> Print = (n) => Console.WriteLine($"Sir {n}");
+            string[] names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            foreach (string name in names)
+            {
+                Print(name);
+            }
+
+
+        }
+    }
+}
