5d70e5127a9b56cd4bb2c76e2f3607ca8da93837
Georgi Georgiev <joro74@gmail.com>
2023-07-20 08:33:53 +0300

    Add files via upload

---
+++ CSharp-Advanced/Functional Programming - Exercise/03. Custom Min Function.cs
@@ -0,0 +1,16 @@
+﻿namespace _03._Custom_Min_Function
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<List<int>, List<int>> minValue = (n) => n.OrderBy(x => x).ToList();
+            List<int> numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+            int result = minValue(numbers)[0];
+            Console.WriteLine(result);
+        }
+    }
+}
