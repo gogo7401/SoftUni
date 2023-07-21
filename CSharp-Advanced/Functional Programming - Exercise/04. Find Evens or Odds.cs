7a7e75e9c153ae21739e75e6afc801799c0fd129
Georgi Georgiev <joro74@gmail.com>
2023-07-21 08:37:51 +0300

    Add files via upload

---
+++ CSharp-Advanced/Functional Programming - Exercise/04. Find Evens or Odds.cs
@@ -0,0 +1,30 @@
+﻿namespace _04._Find_Evens_or_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Predicate<int> isEven = (c) => c % 2 == 0;
+            Predicate<int> isOdd = (d) => d % 2 != 0;
+
+            int[] range = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse) 
+                .ToArray();
+
+            string command = Console.ReadLine();
+
+            for (int i = range[0]; i <= range[1]; i++)
+            {
+                if (command == "even" && isEven(i))
+                {
+                    Console.Write($"{i} ");
+                }
+                else if (command == "odd" && isOdd(i))
+                {
+                    Console.Write($"{i} ");
+                }
+            }
+        }
+    }
+}
