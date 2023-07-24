9d20a3897360f543857ed42eb28183a01cb319a2
Georgi Georgiev <joro74@gmail.com>
2023-07-24 08:55:13 +0300

    Add files via upload

---
+++ CSharp-Advanced/Functional Programming - Exercise/07. Predicate For Names.cs
@@ -0,0 +1,26 @@
+﻿namespace _07._Predicate_For_Names
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int letterCount = int.Parse(Console.ReadLine());    
+            List<string> names = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .ToList();
+
+            Action<List<string>> Print = (list) =>
+            {
+                foreach (var item in list)
+                {
+                    if (item.Length <= letterCount)
+                    {
+                        Console.WriteLine(item);
+                    }
+                }
+            };
+
+            Print(names);
+        }
+    }
+}
