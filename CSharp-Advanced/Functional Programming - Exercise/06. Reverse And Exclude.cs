350988ee61e5d068527f835b4d8e992f22f46f65
Georgi Georgiev <joro74@gmail.com>
2023-07-24 08:54:37 +0300

    Add files via upload

---
+++ CSharp-Advanced/Functional Programming - Exercise/06. Reverse And Exclude.cs
@@ -0,0 +1,29 @@
+﻿namespace _06._Reverse_And_Exclude
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Func<IEnumerable<int>, IEnumerable<int>> reverseList = (x) => x = x.Reverse();
+
+            List<int> list = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToList();
+
+            int divider = int.Parse(Console.ReadLine());
+
+            list = reverseList(list).ToList();
+
+            Predicate<int> isDivide = (x) => x % divider != 0;
+
+            foreach (int item in list)
+            {
+                if (isDivide(item))
+                {
+                    Console.Write($"{item} ");
+                }
+            }
+        }
+    }
+}
