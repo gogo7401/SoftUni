9446b7f132bf31d28124d92fcff36494fa4987f9
Georgi Georgiev <joro74@gmail.com>
2023-07-24 08:56:43 +0300

    Add files via upload

---
+++ CSharp-Advanced/01. Action Print.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading.Channels;
+
+namespace _01._Action_Print
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Action<string[]> Print = (i) => Console.WriteLine(string.Join(Environment.NewLine, i));
+            string[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            Print(input);
+        }
+    }
+}
