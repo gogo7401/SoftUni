0459e1e3103bbfae035a7a2826b5e0438eaf1d9c
Georgi Georgiev <joro74@gmail.com>
2023-08-11 09:31:37 +0300

    Add files via upload

---
+++ CSharp-Advanced/Iterators and Comparators - Lab/01_Library/Library.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+
+namespace IteratorsAndComparators
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+        public Library(params Book[] books)
+        {
+            this.books = books.ToList();
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+
+        class LibraryIterator : IEnumerator<Book>
+        {
+            private List<Book> books;
+
+            private int currentIndex = -1;
+
+            public LibraryIterator(List<Book> books)
+            {
+                Reset();
+                this.books = books;
+                //this.books = new SortedSet<Book>(books).ToList(); // za zadacha 3
+                this.books.Sort(new BookComparator());  // za zadacha 4
+            }
+
+            public Book Current => books[currentIndex];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose()
+            {
+
+            }
+
+            public bool MoveNext()
+            {
+                return ++currentIndex < books.Count;
+            }
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+        }
+
+    }
+
+     
+}
