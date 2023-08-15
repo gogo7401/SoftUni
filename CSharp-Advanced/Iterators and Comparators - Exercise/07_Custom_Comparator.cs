924b11053afbd54aebe3ff0ec053bd9bf6912c74
Georgi Georgiev <joro74@gmail.com>
2023-08-15 11:08:17 +0300

    Add files via upload

---
+++ CSharp-Advanced/Iterators and Comparators - Exercise/07_Custom_Comparator.cs
@@ -0,0 +1,40 @@
+﻿namespace _07_Custom_Comparator
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Array.Sort(numbers, new CustomCompatator());
+
+            Console.WriteLine(string.Join(" ", numbers));
+        }
+
+        
+    }
+    public class CustomCompatator : IComparer<int>
+    {
+        public int Compare(int x, int y)
+        {
+            if (x % 2 == 0 && y % 2 != 0)
+            {
+                return -1;
+            }
+
+            if (x % 2 != 0 && y % 2 == 0)
+            {
+                return 1;
+            }
+
+            return x.CompareTo(y);
+        }
+    }
+
+
+    
+
+}
