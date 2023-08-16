04462c7cd9aed7ca820ed8e8f053cba454c3eaef
Georgi Georgiev <joro74@gmail.com>
2023-08-16 08:50:49 +0300

    Add files via upload

---
+++ CSharp-Advanced/Basic Algorithms - Exercise/01.RecursiveArraySum.cs
@@ -0,0 +1,28 @@
+﻿namespace _1.RecursiveArraySum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+            Console.WriteLine(Sum(arr, 0));
+        }
+
+
+        private static int Sum(int[] array, int index)
+        {
+            if (index >= array.Length)
+            {
+                return 0;
+            }
+
+            int sum = array[index] + Sum(array, index + 1);
+
+            return sum;
+        }
+    }
+}
