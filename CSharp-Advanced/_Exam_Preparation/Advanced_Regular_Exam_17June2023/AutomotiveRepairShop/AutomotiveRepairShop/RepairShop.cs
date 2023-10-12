42e4309f98c75d4a02ff0de489c5975238ed35c8
Georgi Georgiev <joro74@gmail.com>
2023-10-12 13:38:47 +0300

    Add files via upload

---
+++ CSharp-Advanced/_Exam_Preparation/Advanced_Regular_Exam_17June2023/AutomotiveRepairShop/AutomotiveRepairShop/RepairShop.cs
@@ -0,0 +1,55 @@
+﻿using System.Text;
+
+namespace AutomotiveRepairShop
+{
+    public class RepairShop
+    {
+        public RepairShop(int capacity)
+        {
+            Capacity = capacity;
+            Vehicles = new List<Vehicle>();
+        }
+
+        public int Capacity { get; set; }
+
+        public List<Vehicle> Vehicles { get; set; }
+
+
+        public void AddVehicle(Vehicle vehicle)
+        {
+            if (Capacity > Vehicles.Count)
+            {
+                Vehicles.Add(vehicle);
+            }
+        }
+
+        public bool RemoveVehicle(string vin)
+        {
+            return Vehicles.Remove(Vehicles.FirstOrDefault(x => x.VIN == vin));
+        }
+
+        public int GetCount()
+        {
+            return Vehicles.Count;
+        }
+
+        public Vehicle GetLowestMileage()
+        {
+            int min = Vehicles.Min(x => x.Mileage);
+            return Vehicles.FirstOrDefault(x => x.Mileage == min);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine("Vehicles in the preparatory:");
+            foreach (var car in Vehicles)
+            {
+                sb.AppendLine(car.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
