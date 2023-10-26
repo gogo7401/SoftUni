24ad8843efc00a0c1e53bf1fe8d20317bd27ef4f
Georgi Georgiev <joro74@gmail.com>
2023-10-26 08:49:57 +0300

    Add files via upload

---
+++ CSharp-Advanced/_Exam_Preparation/Advanced_Exam_25June2022/01. Tiles Master/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Tiles_Master
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var whiteTiles = new Stack<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+            var greyTiles = new Queue<int>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse));
+
+
+            var location = new Dictionary<int, string>()
+            {
+                { 40, "Sink" },
+                { 50, "Oven"},
+                { 60, "Countertop"},
+                { 70, "Wall"},
+            };
+
+
+            var kitchen = new Dictionary<string, int>();
+
+            while (whiteTiles.Count > 0 && greyTiles.Count > 0)
+            {
+                int white = whiteTiles.Pop();
+                int grey = greyTiles.Dequeue();
+                int largerTile = white + grey;
+
+                if (white == grey)
+                {
+                    if (location.ContainsKey(largerTile))
+                    {
+                        if (kitchen.ContainsKey(location[largerTile]) == false)
+                        {
+                            kitchen.Add(location[largerTile], 0);
+                        }
+                        kitchen[location[largerTile]]++;
+                    }
+                    else
+                    {
+                        if (kitchen.ContainsKey("Floor") == false)
+                        {
+                            kitchen.Add("Floor", 0);
+
+                        }
+                        kitchen["Floor"]++;
+                    }
+                }
+                else
+                {
+                    white /= 2;
+                    whiteTiles.Push(white);
+                    greyTiles.Enqueue(grey);
+                }
+
+
+                
+
+            }
+
+            if (whiteTiles.Count > 0)
+            {
+                Console.WriteLine($"White tiles left: {string.Join(", ", whiteTiles)}");
+            }
+            else
+            {
+                Console.WriteLine("White tiles left: none");
+            }
+
+            if (greyTiles.Count > 0)
+            {
+                Console.WriteLine($"Grey tiles left: {string.Join(", ", greyTiles)}");
+            }
+            else
+            {
+                Console.WriteLine("Grey tiles left: none");
+            }
+
+            foreach (var item in kitchen.OrderByDescending(t => t.Value).ThenBy(t => t.Key))
+            {
+                Console.WriteLine($"{item.Key}: {item.Value}");
+            }
+
+        }
+    }
+}
---
+++ CSharp-Advanced/_Exam_Preparation/Advanced_Exam_25June2022/03.Renovators/Catalog.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Renovators
+{
+    public class Catalog
+    {
+
+        private readonly List<Renovator> renovators;
+
+        public Catalog(string name, int neededRenovators, string project)
+        {
+            Name = name;
+            NeededRenovators = neededRenovators;
+            Project = project;
+            renovators = new List<Renovator>();
+        }
+
+        public string Name { get; set; }
+
+        public int NeededRenovators { get; set; }
+        public string Project { get; set; }
+        public int Count => renovators.Count;
+        public string AddRenovator(Renovator renovator)
+        {
+            if (string.IsNullOrEmpty(renovator.Name) || string.IsNullOrEmpty(renovator.Type))
+            {
+                return "Invalid renovator's information.";
+            }
+            if (NeededRenovators <= renovators.Count)
+            {
+                return "Renovators are no more needed.";
+            }
+
+            if (renovator.Rate > 350)
+            {
+                return "Invalid renovator's rate.";
+            }
+
+            renovators.Add(renovator);
+            return $"Successfully added {renovator.Name} to the catalog.";
+
+        }
+
+        public bool RemoveRenovator(string name)
+        {
+            return renovators.Remove(renovators.FirstOrDefault(r => r.Name == name));
+        }
+
+        public int RemoveRenovatorBySpecialty(string type)
+        {
+            var list = renovators.FindAll(r => r.Type == type);
+
+            foreach (var item in list)
+            {
+                renovators.Remove(item);
+            }
+
+            return list.Count;
+        }
+
+        public Renovator HireRenovator(string name)
+        {
+            var item = renovators.FirstOrDefault(r => r.Name == name);  
+            if (item == null)
+            {
+                return null;
+            }
+            item.Hired = true;
+            return item;
+        }
+
+        public List<Renovator> PayRenovators(int days)
+        {
+            return  renovators.FindAll(r => r.Days >= days);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"Renovators available for Project {Project}:");
+            foreach (var item in renovators.FindAll(r => r.Hired == false))
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
