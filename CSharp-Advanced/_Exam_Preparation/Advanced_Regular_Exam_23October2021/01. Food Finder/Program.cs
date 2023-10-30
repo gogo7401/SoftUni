f1577d91e665a100d6d25e2f2db8424d3926bd09
Georgi Georgiev <joro74@gmail.com>
2023-10-30 08:42:03 +0200

    Add files via upload

---
+++ CSharp-Advanced/_Exam_Preparation/Advanced_Regular_Exam_23October2021/01. Food Finder/Program.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Food_Finder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vowels = new Queue<char>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse));
+
+            var consonants = new Stack<char>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(char.Parse));
+
+
+
+            var words = new Dictionary<string, string>()
+            {
+                { "pear", "pear" },
+                { "flour", "flour"},
+                { "pork", "pork"},
+                { "olive", "olive"},
+            };
+
+            var listWords = new List<string>()
+            {
+                { "pear" },
+                { "flour" },
+                { "pork" },
+                { "olive" },
+
+            };
+
+            while (consonants.Any())
+            {
+                var c = consonants.Pop().ToString();   
+                var v = vowels.Dequeue().ToString();   
+
+                foreach ( var word in listWords)
+                {
+                    if (words[word].Contains(c))
+                    {
+                        words[word] = words[word].Replace(c, string.Empty);
+                    }
+
+                    if (words[word].Contains(v))
+                    {
+                        words[word] = words[word].Replace(v, string.Empty);
+                    }
+                }
+
+                vowels.Enqueue(char.Parse(v));
+
+            }
+
+            Console.WriteLine($"Words found: {words.Where(w => w.Value == string.Empty).Count()}");
+            foreach ( var word in words)
+            {
+                if (word.Value == string.Empty)
+                {
+                    Console.WriteLine(word.Key);
+                }
+            }
+
+
+        }
+
+        
+
+    }
+}
---
+++ CSharp-Advanced/_Exam_Preparation/Advanced_Regular_Exam_23October2021/03._Stock_Market/StockMarket/Investor.cs
@@ -0,0 +1,82 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace StockMarket
+{
+    public class Investor
+    {
+        public Investor(string fullName, string emailAddress, decimal moneyToInvest, string brokerName)
+        {
+            FullName = fullName;
+            EmailAddress = emailAddress;
+            MoneyToInvest = moneyToInvest;
+            BrokerName = brokerName;
+            Portfolio = new List<Stock>();
+        }
+
+        private readonly List<Stock> Portfolio;
+        public string FullName { get; set; }
+        public string EmailAddress { get; set; }
+        public decimal MoneyToInvest { get; set; }
+        public string BrokerName { get; set; }
+
+        public int Count => Portfolio.Count;
+
+        public void BuyStock(Stock stock)
+        {
+            if (stock.MarketCapitalization > 10000 && this.MoneyToInvest >= stock.PricePerShare)
+            {
+                Portfolio.Add(stock);
+                this.MoneyToInvest -= stock.PricePerShare;
+            }
+
+        }
+
+        public string SellStock(string companyName, decimal sellPrice)
+        {
+            if (Portfolio.FirstOrDefault(x => x.CompanyName == companyName) == null)
+            {
+                return $"{companyName} does not exist.";
+            }
+
+            var company = Portfolio.FirstOrDefault(x => x.CompanyName == companyName);
+
+            if (sellPrice < company.PricePerShare)
+            {
+                return $"Cannot sell {companyName}.";
+            }
+
+            MoneyToInvest += sellPrice;//company.PricePerShare;
+            Portfolio.Remove(company);
+            return $"{companyName} was sold.";
+
+        }
+
+        public Stock FindStock(string companyName)
+        {
+            return Portfolio.FirstOrDefault(x => x.CompanyName == companyName);
+        }
+
+        public Stock FindBiggestCompany()
+        {
+            var max = Portfolio.Max(x => x.MarketCapitalization);
+            return Portfolio.FirstOrDefault(x => x.MarketCapitalization == max);
+        }
+
+        public string InvestorInformation()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"The investor {FullName} with a broker {BrokerName} has stocks:");
+            foreach (var item in Portfolio)
+            {
+                sb.AppendLine(item.ToString());
+            }
+
+
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
