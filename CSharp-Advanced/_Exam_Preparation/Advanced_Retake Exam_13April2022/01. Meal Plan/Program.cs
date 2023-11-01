ef5b094c8a64513e4dde0488c42a063e13564bd6
Georgi Georgiev <joro74@gmail.com>
2023-11-01 11:50:34 +0200

    Add files via upload

---
+++ CSharp-Advanced/_Exam_Preparation/Advanced_Retake Exam_13April2022/01. Meal Plan/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Meal_Plan
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var meals = new Queue<string>(Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries));
+
+            var caloriesPerDay = new Stack<int>(Console.ReadLine()
+               .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+               .Select(int.Parse));
+
+
+
+            var mealCalories = new Dictionary<string, int>()
+            {
+                { "salad", 350 },
+                { "soup", 490 },
+                { "pasta", 680 },
+                { "steak", 790 },
+            };
+
+            int numberOfMeals = meals.Count;
+            int remainder = 0;
+
+            while (meals.Count > 0 && caloriesPerDay.Count > 0)
+            {
+                string food = meals.Dequeue();
+                int foodCalories = mealCalories[food];
+                int calForDay = caloriesPerDay.Pop();
+
+                //calForDay += remainder;
+
+                if (calForDay - foodCalories >= 0)
+                {
+                    calForDay -= foodCalories;
+                    if (calForDay > 0) { caloriesPerDay.Push(calForDay); }
+
+                }
+                else
+                {
+                    remainder = Math.Abs(calForDay - foodCalories);
+                    if (caloriesPerDay.Any())
+                    {
+                        int cal = caloriesPerDay.Pop();
+                        cal -= remainder;
+                        caloriesPerDay.Push(cal);
+                    }
+
+                }
+
+
+
+            }
+
+            if (meals.Count == 0)
+            {
+                Console.WriteLine($"John had {numberOfMeals} meals.");
+                Console.WriteLine($"For the next few days, he can eat {string.Join(", ", caloriesPerDay)} calories.");
+            }
+            else
+            {
+                Console.WriteLine($"John ate enough, he had {numberOfMeals - meals.Count} meals.");
+                Console.WriteLine($"Meals left: {string.Join(", ", meals)}.");
+            }
+        }
+    }
+}
---
+++ CSharp-Advanced/_Exam_Preparation/Advanced_Retake Exam_13April2022/03.Zoo/Zoo.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Zoo
+{
+    public class Zoo
+    {
+        //private readonly List<Animal> Animals;
+
+        public Zoo(string name, int capacity)
+        {
+            Name = name;
+            Capacity = capacity;
+            Animals = new List<Animal>();
+        }
+
+        public List<Animal> Animals { get; }
+
+        public string Name { get; set; }
+        public int Capacity { get; set; }
+
+        public string AddAnimal(Animal animal)
+        {
+            if (string.IsNullOrWhiteSpace(animal.Species))
+            {
+                return "Invalid animal species.";
+            }
+
+            if (animal.Diet != "herbivore" && animal.Diet != "carnivore")
+            {
+                return "Invalid animal diet.";
+            }
+
+            if (Animals.Count == Capacity)
+            {
+                return "The zoo is full.";
+            }
+
+            Animals.Add(animal);
+            return $"Successfully added {animal.Species} to the zoo.";
+        }
+
+        public int RemoveAnimals(string species)
+        {
+            var list = Animals.FindAll(x => x.Species == species);
+
+            foreach (var animal in list)
+            {
+                Animals.Remove(animal);
+            }
+
+            return list.Count;
+        }
+
+        public List<Animal> GetAnimalsByDiet(string diet)
+        {
+            return Animals.FindAll(a => a.Diet == diet);
+        }
+
+        public Animal GetAnimalByWeight(double weight)
+        {
+            return Animals.FirstOrDefault(a => a.Weight == weight); 
+        }
+
+        public string GetAnimalCountByLength(double minimumLength, double maximumLength)
+        {
+            var list = Animals.FindAll(a => a.Length >= minimumLength && a.Length <= maximumLength);
+
+            return $"There are {list.Count} animals with a length between {minimumLength} and {maximumLength} meters.";
+        }
+    }
+}
