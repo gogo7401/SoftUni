2d8348d5417387e289ab744c3c02d9bd2e05cf28
Georgi Georgiev <joro74@gmail.com>
2023-11-08 10:40:56 +0200

    Add files via upload

---
+++ CSharp-Advanced/_Exam_Preparation/Advanced_Retake_Exam_18August2022/01. Barista Contest/Program.cs
@@ -0,0 +1,82 @@
+﻿namespace _01._Barista_Contest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var coffeeDrinks = new Dictionary<int, string>()
+            {
+                { 50, "Cortado" },
+                { 75, "Espresso"},
+                { 100, "Capuccino"},
+                { 150, "Americano"},
+                { 200, "Latte"},
+            };
+
+
+            var createdDrinks = new Dictionary<string, int>();
+
+            var coffee = new Queue<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+            var milk = new Stack<int>(Console.ReadLine().Split(", ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse));
+
+            int sum = 0;
+
+            while (true)
+            {
+                if (coffee.Count == 0 || milk.Count == 0) 
+                {
+                    break;
+                }
+                int currMilk = milk.Peek(); 
+                sum = coffee.Dequeue() + milk.Pop();
+
+                if (coffeeDrinks.ContainsKey(sum))
+                {
+                    if (createdDrinks.ContainsKey(coffeeDrinks[sum]) == false)
+                    {
+                        createdDrinks.Add(coffeeDrinks[sum],0);
+                    }
+                    createdDrinks[coffeeDrinks[sum]]++;
+                }
+                else
+                {
+                    milk.Push(currMilk-5);
+                }
+
+            }
+
+            if (coffee.Count == 0 && milk.Count == 0)
+            {
+                Console.WriteLine("Nina is going to win! She used all the coffee and milk!");
+            }
+            else
+            {
+                Console.WriteLine("Nina needs to exercise more! She didn't use all the coffee and milk!");
+            }
+
+            if (coffee.Count == 0)
+            {
+                Console.WriteLine("Coffee left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Coffee left: {string.Join(", ", coffee)}");
+            }
+
+            if (milk.Count == 0)
+            {
+                Console.WriteLine("Milk left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Milk left: {string.Join(", ", milk)}");
+            }
+
+            foreach (var drink in createdDrinks.OrderBy(d => d.Value).ThenByDescending(d => d.Key))
+            {
+                Console.WriteLine($"{drink.Key}: {drink.Value}");
+            }
+
+        }
+    }
+}
---
+++ CSharp-Advanced/_Exam_Preparation/Advanced_Retake_Exam_18August2022/03.Basketball/Team.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace Basketball
+{
+    public class Team
+    {
+        private readonly List<Player> players;
+
+        public Team(string name, int openPositions, char group)
+        {
+            Name = name;
+            OpenPositions = openPositions;
+            Group = group;
+            players = new List<Player>();
+        }
+
+        public string Name { get; set; }
+        public int OpenPositions { get; set; }
+        public char Group { get; set; }
+        public int Count => players.Count;
+
+        public string AddPlayer(Player player)
+        {
+            if (string.IsNullOrEmpty(player.Name) || string.IsNullOrEmpty(player.Position))
+            {
+                return "Invalid player's information.";
+            }
+
+            if (OpenPositions == 0)
+            {
+                return "There are no more open positions.";
+            }
+
+            if (player.Rating < 80)
+            {
+                return "Invalid player's rating.";
+            }
+
+            players.Add(player);
+            OpenPositions--;
+
+            return $"Successfully added {player.Name} to the team. Remaining open positions: {OpenPositions}.";
+        }
+
+        public bool RemovePlayer(string name)
+        {
+            bool removed = false;
+
+            removed = players.Remove(players.FirstOrDefault(n => n.Name == name));
+
+            if (removed) { OpenPositions++; }
+
+            return removed;
+        }
+
+        public int RemovePlayerByPosition(string position)
+        {
+            int removedCount = 0;
+
+            var list = players.FindAll(n => n.Position == position);
+
+            removedCount = list.Count;
+
+            OpenPositions += removedCount;
+
+            foreach (var player in list)
+            {
+                players.Remove(player);
+            }
+
+            return removedCount;
+        }
+
+        public Player RetirePlayer(string name)
+        {
+            Player retiredPlayer = players.FirstOrDefault(n => n.Name == name);
+
+            if (retiredPlayer != null)
+            {
+                retiredPlayer.Retired = true;
+            }
+
+            return retiredPlayer;
+        }
+
+        public List<Player> AwardPlayers(int games)
+        {
+            return players.FindAll(n => n.Games >= games);
+        }
+
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+
+            sb.AppendLine($"Active players competing for Team {Name} from Group {Group}:");
+
+            var list = players.FindAll(p => p.Retired == false);
+
+            foreach ( var player in list)
+            {
+                sb.AppendLine(player.ToString());
+            }
+
+
+            return sb.ToString().TrimEnd();
+        }
+
+    }
+}
