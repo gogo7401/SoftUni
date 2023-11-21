c391257df304f883f6d2d6575b647c7f57f7689b
Georgi Georgiev <joro74@gmail.com>
2023-11-21 13:59:43 +0200

    Delete CSharp-OOP/_ExamPreparation/0.TipsTricks directory

--- CSharp-OOP/_ExamPreparation/0.TipsTricks/Engine.cs
+++
@@ -1,7 +0,0 @@
-﻿        public Engine()
-        {
-			// Това се пише за да печата във файл
-            this.writer = new FileWriter();
-            this.reader = new Reader();
-            this.controller = new Controller();
-        }
