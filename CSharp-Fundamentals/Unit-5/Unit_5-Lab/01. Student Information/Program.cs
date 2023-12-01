e95aef46bcbf672e5f12fcd320415c86577443b3
Georgi Georgiev <joro74@gmail.com>
2023-12-01 13:20:57 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-5/Unit_5-Lab/01. Student Information/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace _01._Student_Information
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string studentName = Console.ReadLine();
+            string age = Console.ReadLine();
+            string averageGrade = Console.ReadLine();
+
+            Console.WriteLine($"Name: {studentName}, Age: {age}, Grade: {averageGrade}");
+        }
+    }
+}
---
+++ CSharp-Fundamentals/Unit-5/Unit_5-Lab/09. Sum of Odd Numbers/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _09._Sum_of_Odd_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int sum = 0;
+            int number = 1;
+
+            for (int i = 1; i <= n; i++)
+            {
+                sum += number;
+                Console.WriteLine(number);
+                number += 2;
+            }
+            Console.WriteLine($"Sum: {sum}");
+        }
+    }
+}
