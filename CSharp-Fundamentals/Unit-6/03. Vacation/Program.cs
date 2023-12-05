d1bc0f7727a4a5856485f3c5e0e65f6c34a2f4cf
Georgi Georgiev <joro74@gmail.com>
2023-12-05 08:37:42 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-6/03. Vacation/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+
+namespace _03._Vacation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int peopleCount = int.Parse(Console.ReadLine());
+            string groupType = Console.ReadLine();
+            string weekDay = Console.ReadLine();
+            double singlePersonPrice = 0.00;
+            double totalPrice = 0.00;
+
+            switch (weekDay)
+            {
+                case "Friday":
+                    if (groupType == "Students")
+                    {
+                        singlePersonPrice = 8.45;
+                    }
+                    else if (groupType == "Business")
+                    {
+                        singlePersonPrice = 10.90;
+                    }
+                    else if (groupType == "Regular")
+                    {
+                        singlePersonPrice = 15.00;
+                    }
+                    break;
+                case "Saturday":
+                    if (groupType == "Students")
+                    {
+                        singlePersonPrice = 9.80;
+                    }
+                    else if (groupType == "Business")
+                    {
+                        singlePersonPrice = 15.60;
+                    }
+                    else if (groupType == "Regular")
+                    {
+                        singlePersonPrice = 20.00;
+                    }
+                    break;
+                case "Sunday":
+                    if (groupType == "Students")
+                    {
+                        singlePersonPrice = 10.46;
+                    }
+                    else if (groupType == "Business")
+                    {
+                        singlePersonPrice = 16.00;
+                    }
+                    else if (groupType == "Regular")
+                    {
+                        singlePersonPrice = 22.50;
+                    }
+                    break; 
+ 
+            }
+            if (groupType == "Students" && peopleCount >= 30)
+            {
+                totalPrice = singlePersonPrice * peopleCount - (singlePersonPrice * peopleCount * 0.15);
+            }
+            else if (groupType == "Business" && peopleCount >= 100)
+            {
+                totalPrice = singlePersonPrice * (peopleCount - 10);
+            }
+            else if (groupType == "Regular" && peopleCount >= 10 && peopleCount <= 20) 
+            {
+                totalPrice = singlePersonPrice * peopleCount - (singlePersonPrice * peopleCount * 0.05);
+            }
+            else { totalPrice = singlePersonPrice * peopleCount; }
+            Console.WriteLine($"Total price: {totalPrice:F2}");
+        }
+    }
+}
---
+++ CSharp-Fundamentals/Unit-6/04. Print and sum/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _04._Print_and_sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int startnumber = int.Parse(Console.ReadLine());    
+            int endNumber = int.Parse(Console.ReadLine());
+            int sum = 0;
+            string pattern = "";
+
+            for (int i = startnumber; i <= endNumber; i++)
+            {
+                sum += i;
+                pattern += (i + " ");
+            }
+            Console.WriteLine(pattern);
+            Console.WriteLine("Sum: " + sum);
+        }
+    }
+}
