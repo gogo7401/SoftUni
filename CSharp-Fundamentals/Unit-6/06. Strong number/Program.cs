675b3b3c55ffa232cacf22f6d684fade1b955495
Georgi Georgiev <joro74@gmail.com>
2023-12-07 10:23:24 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-6/06. Strong number/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace _06._Strong_number
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string strongNumber = Console.ReadLine();
+            int sum = 0;
+            int fact = 1;
+
+            for (int i = 0; i < strongNumber.Length; i++)
+            {
+                
+                for (int j = 1; j <= int.Parse(strongNumber[i].ToString()); j++)
+                {
+                    fact *= j;
+                }
+                sum += fact;
+                fact = 1;
+            }
+            if (sum == int.Parse(strongNumber.ToString()))
+            {
+                Console.WriteLine("yes");
+                //Console.WriteLine(sum);
+            }
+            else 
+            { 
+                Console.WriteLine("no");
+                //Console.WriteLine(sum);
+            }
+        }
+
+    }
+}
