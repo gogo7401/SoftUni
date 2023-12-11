2c68eec110a6e0dcf3f32ce89097e906c42e2e52
Georgi Georgiev <joro74@gmail.com>
2023-12-11 15:09:10 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-6/09. Padawan Equipment/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace _09._Padawan_Equipment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double amountOfMoney = double.Parse(Console.ReadLine());
+            int countOfStudents = int.Parse(Console.ReadLine());
+            double priceOfLightsabers = double.Parse(Console.ReadLine());
+            double priceOfRobes = double.Parse(Console.ReadLine());
+            double priceOfBelts = double.Parse(Console.ReadLine());
+
+            int lightsabresCount = int.Parse(Math.Ceiling(countOfStudents * 1.1).ToString());
+            int beltCount = countOfStudents - (countOfStudents / 6);
+
+            double equipmentPrice = lightsabresCount * priceOfLightsabers + countOfStudents * priceOfRobes + beltCount * priceOfBelts;
+
+            if (equipmentPrice <= amountOfMoney)
+            {
+                Console.WriteLine($"The money is enough - it would cost {equipmentPrice:F2}lv.");
+            }
+            else { Console.WriteLine($"John will need {equipmentPrice-amountOfMoney:F2}lv more."); }
+        }
+    }
+}
