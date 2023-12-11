766c528b72e882bc56b2eef105f854bf8a2523ba
Georgi Georgiev <joro74@gmail.com>
2023-12-11 15:09:23 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-6/10. Rage Expenses/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace _10._Rage_Expenses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int lostGamesCount = int.Parse(Console.ReadLine());
+            double headsetPrice = double.Parse(Console.ReadLine());
+            double mousePrice = double.Parse(Console.ReadLine());
+            double keyboardPrice = double.Parse(Console.ReadLine());
+            double displayPrice = double.Parse(Console.ReadLine());
+
+            int headsetCount = lostGamesCount / 2;
+            int mouseCount = lostGamesCount / 3;
+            int keyboardCount = lostGamesCount / 6;
+            int displayCount = lostGamesCount / 12;
+
+            double expenses = headsetCount * headsetPrice + mouseCount * mousePrice + keyboardCount * keyboardPrice + displayCount * displayPrice;
+
+            Console.WriteLine($"Rage expenses: {expenses:F2} lv.");
+        }
+    }
+}
