5a88225fe37e84ffbf4cca579c3d235d0bd3b8c4
Georgi Georgiev <joro74@gmail.com>
2023-12-14 11:29:05 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-8/05. Special Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace _05._Special_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int inputNumber = int.Parse(Console.ReadLine());
+            string inputNumberString;
+            int sum = 0;    
+
+            for (int c = 1; c <= inputNumber; c++)
+            {
+                inputNumberString = c.ToString();
+                sum = 0;
+                for (int i = 0; i < inputNumberString.Length; i++)
+                {
+                    sum += int.Parse(inputNumberString[i].ToString());  
+                }
+                if (sum == 5 || sum == 7 || sum == 11)
+                {
+                    Console.WriteLine($"{c} -> True");
+                }
+                else { Console.WriteLine($"{c} -> False"); }
+            }
+            
+        }
+    }
+}
