a2516cb99f9b562bda57c73479115bcab3c1585b
Georgi Georgiev <joro74@gmail.com>
2023-12-21 08:56:52 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-9/07. Water Overflow/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _07._Water_Overflow
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int volumeOfTank = int.Parse(Console.ReadLine());
+
+            int sum = 0;
+
+            for (int i = 0; i < volumeOfTank; i++)
+            {
+                int litres = int.Parse(Console.ReadLine());
+
+                sum += litres;
+
+                if (sum > 255)
+                {
+                    sum -= litres;
+                    Console.WriteLine("Insufficient capacity!");
+                }
+
+            }
+            Console.WriteLine(sum);
+        }
+    }
+}
