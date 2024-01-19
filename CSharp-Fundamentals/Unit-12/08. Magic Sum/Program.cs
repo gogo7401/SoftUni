6f1df10cb547974847fc01cccc2dfa3cf993609f
Georgi Georgiev <joro74@gmail.com>
2024-01-19 08:35:53 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-12/08. Magic Sum/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Linq;
+
+namespace _08._Magic_Sum
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] line = Console.ReadLine().Split().Select(int.Parse).ToArray();    
+            int magicNumber = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < line.Length; i++)
+            {
+                for (int j = i+1; j < line.Length; j++)
+                {
+                    if ((line[i] + line[j] ) == magicNumber)
+                    {
+                        Console.WriteLine($"{line[i]} {line[j]}");
+                    }
+                }
+            }
+        }
+    }
+}
