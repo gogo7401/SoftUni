46acfdb523d4823a3a9618c846fe9be8d5e6b1cb
Georgi Georgiev <joro74@gmail.com>
2024-01-23 08:35:06 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-14/03. Calculations/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace _03._Calculations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string operation = Console.ReadLine();
+            double firstNumber = double.Parse(Console.ReadLine());
+            double secondNumber = double.Parse(Console.ReadLine());
+
+            switch (operation)
+            {
+                case "add":
+                    PrintAdd(firstNumber, secondNumber);
+                    break;
+                case "multiply":
+                    PrintMultiply(firstNumber, secondNumber);
+                    break;
+                case "subtract":
+                    PrintSubtract(firstNumber, secondNumber);
+                    break;
+                case "divide":
+                    PrintDivide(firstNumber, secondNumber);
+                    break;
+            }
+        }
+
+        static void PrintAdd(double firstNumber, double secondNumber)
+        {
+            Console.WriteLine(firstNumber+secondNumber);
+        }
+
+        static void PrintMultiply(double firstNumber, double secondNumber)
+        {
+            Console.WriteLine(firstNumber * secondNumber);
+        }
+
+        static void PrintSubtract(double firstNumber, double secondNumber)
+        {
+            Console.WriteLine(firstNumber - secondNumber);
+        }
+
+        static void PrintDivide(double firstNumber, double secondNumber)
+        {
+            if (secondNumber > 0)
+            {
+                Console.WriteLine(firstNumber / secondNumber);
+            }
+            
+        }
+    }
+}
