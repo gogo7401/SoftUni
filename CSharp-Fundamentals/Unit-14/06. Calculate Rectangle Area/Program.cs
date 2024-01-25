3346cb3fb8c288cbd20e5eda79cebf0e0efbc4f4
Georgi Georgiev <joro74@gmail.com>
2024-01-25 08:34:11 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-14/06. Calculate Rectangle Area/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _06._Calculate_Rectangle_Area
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double height = double.Parse(Console.ReadLine());
+            double width = double.Parse(Console.ReadLine());
+            double area = GetRectangleArea(height, width);
+            Console.WriteLine(area);
+
+        }
+
+        static double GetRectangleArea(double height, double width)
+        {
+            double result = height * width;
+
+            return result;
+        }
+    }
+}
