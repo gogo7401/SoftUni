3c0333ecd3aa87eecf03205ec9ccdd636aa31a78
Georgi Georgiev <joro74@gmail.com>
2024-01-26 13:24:51 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-14/10. Multiply Evens by Odds/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+
+namespace _10._Multiply_Evens_by_Odds
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+            int inputNumber = Math.Abs(int.Parse(input));
+
+            int sumOfEvenDigits = GetSumOfEvenDigits(inputNumber.ToString());
+            int sumOfOddDigits = GetSumOfOddDigits(inputNumber.ToString());
+            GetMultipleOfEvenAndOdds(sumOfEvenDigits, sumOfOddDigits);
+
+        }
+
+        private static void GetMultipleOfEvenAndOdds(int sumOfEvenDigits, int sumOfOddDigits)
+        {
+            int result = 0;
+            result = sumOfEvenDigits * sumOfOddDigits;
+            Console.WriteLine(result);
+        }
+
+        private static int GetSumOfOddDigits(string input)
+        {
+            int result = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] % 2 != 0)
+                {
+                    result += int.Parse(input[i].ToString());
+                }
+            }
+            return result;
+        }
+
+        private static int GetSumOfEvenDigits(string input)
+        {
+            int result = 0;
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] % 2 == 0)
+                {
+                    result +=  int.Parse(input[i].ToString());
+                }
+            }
+            return result;
+        }
+    }
+}
