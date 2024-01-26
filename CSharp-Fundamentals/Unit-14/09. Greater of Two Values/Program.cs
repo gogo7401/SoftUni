a2166fa58cc61358671ea619759ed7fbef9edeeb
Georgi Georgiev <joro74@gmail.com>
2024-01-26 13:24:35 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-14/09. Greater of Two Values/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace _09._Greater_of_Two_Values
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputType = Console.ReadLine();
+            string first = Console.ReadLine();
+            string second = Console.ReadLine();
+
+            switch (inputType)
+            {
+                case "int":
+                    int firstInt = int.Parse(first);
+                    int secondInt = int.Parse(second);
+                    GetMax(firstInt, secondInt);
+                    break;
+                case "char":
+                    char firstChar = char.Parse(first);
+                    char secondChar = char.Parse(second);
+                    GetMax(firstChar, secondChar);
+                    break;
+                case "string":
+                    GetMax(first, second);
+                    break;
+            }
+            
+        }
+
+        static void GetMax(int first, int second)
+        {
+            if (first > second)
+            {
+                Console.WriteLine(first);
+            }
+            else { Console.WriteLine(second); }
+        }
+
+        static void GetMax(char first, char second)
+        {
+            if (first > second)
+            {
+                Console.WriteLine(first);
+            }
+            else { Console.WriteLine(second); }
+        }
+
+        static void GetMax(string first, string second)
+        {
+            if (first.CompareTo(second) > 0)
+            {
+                Console.WriteLine(first);
+            }
+            else { Console.WriteLine(second); } 
+        }
+    }
+}
