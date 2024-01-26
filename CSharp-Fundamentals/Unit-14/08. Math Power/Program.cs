df4b603e83a69544678e2db732ed03bf0ccf51e0
Georgi Georgiev <joro74@gmail.com>
2024-01-26 13:24:21 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-14/08. Math Power/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _08._Math_Power
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double baseNumber = double.Parse(Console.ReadLine());
+            int powerNumber = int.Parse(Console.ReadLine());
+
+            double result = GetPower(baseNumber, powerNumber);
+            Console.WriteLine(result);
+        }
+
+        private static double GetPower(double baseNumber, int powerNumber)
+        {
+            double result = 0;
+            result = Math.Pow(baseNumber, powerNumber);
+
+            return result;
+        }
+    }
+}
