02073d6789c3ddd01e3538585c37db87204a6be9
Georgi Georgiev <joro74@gmail.com>
2024-01-26 13:24:09 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-14/07. Repeat String/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace _07._Repeat_String
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();  
+            int count = int.Parse(Console.ReadLine());
+            string result = PrintString(input, count);
+            Console.WriteLine(result);
+
+        }
+
+        static string PrintString(string input, int count)
+        {
+            string result = "";
+            for (int i = 0; i < count; i++)
+            {
+                result += input;
+            }
+            return result;
+        }
+    }
+}
