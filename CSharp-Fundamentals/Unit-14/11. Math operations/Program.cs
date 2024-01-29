057d031dff996256c14c442c34bafb17241a258d
Georgi Georgiev <joro74@gmail.com>
2024-01-29 10:40:06 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-14/11. Math operations/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _11._Math_operations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNumber = double.Parse(Console.ReadLine());
+            string operation = Console.ReadLine();
+            double secondNumber = double.Parse(Console.ReadLine());
+            double result = GetCalcutation(firstNumber, secondNumber, operation);
+            Console.WriteLine(result);
+
+        }
+
+        private static double GetCalcutation(double firstNumber, double secondNumber, string operation)
+        {
+            double result = 0;  
+            switch (operation)
+            {
+                case "+":
+                    result = firstNumber + secondNumber;    
+                    break;
+                case "-":
+                    result = firstNumber - secondNumber;    
+                    break;
+                case "*":
+                    result = firstNumber * secondNumber;
+                    break;
+                case "/":
+                    result = firstNumber / secondNumber;
+                    break;
+            }
+            return result;
+        }
+    }
+}
