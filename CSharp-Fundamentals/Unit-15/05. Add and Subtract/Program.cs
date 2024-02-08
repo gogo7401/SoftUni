658542b38c11a2af9e1ad0281f0b0cf96f381c7d
Georgi Georgiev <joro74@gmail.com>
2024-02-08 08:50:08 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-15/05. Add and Subtract/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace _05._Add_and_Subtract
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int firstNumber = int.Parse(Console.ReadLine());
+            int secondNumber = int.Parse(Console.ReadLine());
+            int thirdNumber = int.Parse(Console.ReadLine());
+
+            int sumOneAndTwo = GetSum(firstNumber, secondNumber);
+            int subtractSumAndThird = GetSubtract(sumOneAndTwo, thirdNumber);
+            Console.WriteLine(subtractSumAndThird);
+        }
+
+
+        private static int GetSum(int firstNumber, int secondNumber)
+        {
+            int sum = firstNumber + secondNumber;
+
+            return sum;
+
+        }
+        private static int GetSubtract(int sumOneAndTwo, int thirdNumber)
+        {
+            int subtract = sumOneAndTwo - thirdNumber;
+
+            return subtract;
+        }
+    }
+}
