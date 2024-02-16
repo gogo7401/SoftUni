0c688c8878bf43bcc363333608b3df519785b4cb
Georgi Georgiev <joro74@gmail.com>
2024-02-16 10:07:10 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-17/01. Sum Adjacent Equal Numbers/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _01._Sum_Adjacent_Equal_Numbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //List<string> input = new List<string>();
+            List <double> input = Console.ReadLine().Split().Select(double.Parse).ToList();
+            for (int i = 0; i < input.Count-1; i++)
+            {
+                if (input[i] == input[i+1])
+                {
+                    input[i] = input [i] + input[i+1];
+                    input.Remove(input[i + 1]);
+                    i = -1;
+                }
+            }
+            Console.WriteLine(string.Join(" ", input));
+
+        }
+    }
+}
---
+++ CSharp-Fundamentals/Unit-17/02. Gauss' Trick/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02._Gauss__Trick
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            List<int> list = Console.ReadLine().Split().Select(int.Parse).ToList();
+            List<int> result = new List<int>();
+            for (int i = 0; i < list.Count/2; i++)
+            {
+                result.Add(list[i] + list[(list.Count-1)-i]);
+            }
+            if (list.Count % 2 != 0)
+            {
+                result.Add(list[list.Count/2]);
+            }
+            Console.WriteLine(string.Join(" ", result));
+        }
+    }
+}
