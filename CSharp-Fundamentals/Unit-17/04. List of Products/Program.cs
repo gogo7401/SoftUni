492af12834873430162f45358bf7ff8f4440c331
Georgi Georgiev <joro74@gmail.com>
2024-02-20 11:03:54 +0200

    Add files via upload

---
+++ CSharp-Fundamentals/Unit-17/04. List of Products/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+
+namespace _04._List_of_Products
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> productsList = new List<string>();
+            for (int i = 0; i < n; i++)
+            {
+                productsList.Add(Console.ReadLine());
+            }
+            productsList.Sort();
+            for (int i = 1; i <= productsList.Count; i++)
+            {
+                Console.WriteLine($"{i}.{productsList[i-1]}");
+            }
+            
+        }
+    }
+}
