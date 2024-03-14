e812f54ba6f27edf168ee6d84493bf6bcb8b23f7
Georgi Georgiev <joro74@gmail.com>
2024-03-14 09:11:14 +0200

    Add files via upload

---
+++ CSharp-Basic/First Steps in Coding - More Exercises/SoftUniPB_03_practice_02/02.cs
@@ -0,0 +1,17 @@
+﻿//  02
+using System;
+
+namespace TriangleArea
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double a = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            double s = a * h / 2;
+
+            Console.WriteLine($"{s:F2}");
+        }
+    }
+}
