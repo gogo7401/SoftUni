9cd8085114c96f75fa681bb214c217027c96dc58
Georgi Georgiev <joro74@gmail.com>
2024-03-18 09:52:26 +0200

    Add files via upload

---
+++ CSharp-Basic/First Steps in Coding - More Exercises/SoftUniPB_03_practice_03/03.cs
@@ -0,0 +1,17 @@
+﻿//  03
+using System;
+
+namespace CelsiustoFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double c = double.Parse(Console.ReadLine());
+            
+            double f = c*1.8 + 32;
+
+            Console.WriteLine($"{f:F2}");
+        }
+    }
+}
