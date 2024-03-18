dd099b2080917b84a2e681bf9af7402cb4b0b172
Georgi Georgiev <joro74@gmail.com>
2024-03-18 09:52:44 +0200

    Add files via upload

---
+++ CSharp-Basic/First Steps in Coding - More Exercises/SoftUniPB_03_practice_04/04.cs
@@ -0,0 +1,21 @@
+﻿//  04
+using System;
+
+namespace CelsiustoFahrenheit
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double priceVegetable = double.Parse(Console.ReadLine());
+            double priceFruite = double.Parse(Console.ReadLine());
+            double kiloVegetable = double.Parse(Console.ReadLine());
+            double kiloFruit = double.Parse(Console.ReadLine());
+
+            double prihodi = ((kiloVegetable* priceVegetable) +(kiloFruit* priceFruite)) / 1.94;
+            
+
+            Console.WriteLine($"{prihodi:F2}");
+        }
+    }
+}
