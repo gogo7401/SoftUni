ae14ce2ca062cd92b1ca9b5e0fb07c3a5ef60947
Georgi Georgiev <joro74@gmail.com>
2024-03-19 13:45:21 +0200

    Add files via upload

---
+++ CSharp-Basic/First Steps in Coding - More Exercises/SoftUniPB_03_practice_05/05.cs
@@ -0,0 +1,21 @@
+﻿//  05
+using System;
+
+namespace TrainingLab
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double w = double.Parse(Console.ReadLine());
+            double h = double.Parse(Console.ReadLine());
+            h = h - 1;
+            double koloni = Math.Truncate( h / 0.7);
+            double redove = Math.Truncate(w/1.2);
+   
+            double rm = koloni * redove - 3;
+
+            Console.WriteLine(rm);
+        }
+    }
+}
