fa2dc11c7950089716cf7a657a227efb3d9fedaa
Georgi Georgiev <joro74@gmail.com>
2024-03-20 15:10:55 +0200

    Add files via upload

---
+++ CSharp-Basic/First Steps in Coding - More Exercises/SoftUniPB_03_practice_06/06.cs
@@ -0,0 +1,26 @@
+﻿//  06
+using System;
+
+namespace Fishland
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double cenaSkumria = double.Parse(Console.ReadLine());
+            double cenaCaca = double.Parse(Console.ReadLine());
+            double kilogramaPalamud = double.Parse(Console.ReadLine());
+            double kilogramaSafrid = double.Parse(Console.ReadLine());
+            int kilogramaMidi = int.Parse(Console.ReadLine());
+
+            double cenaMidi = 7.50;
+            double cenaPalamud = cenaSkumria * 1.6;
+            double cenaSafrid = cenaCaca * 1.8;
+
+            double suma = cenaPalamud * kilogramaPalamud + cenaSafrid * kilogramaSafrid + cenaMidi * kilogramaMidi; 
+
+
+            Console.WriteLine($"{suma:F2}");
+        }
+    }
+}
