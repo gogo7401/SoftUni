23d53963e164c759ca6f536d9af00c6e5c05f448
Georgi Georgiev <joro74@gmail.com>
2024-03-21 09:09:16 +0200

    Add files via upload

---
+++ CSharp-Basic/First Steps in Coding - More Exercises/SoftUniPB_03_practice_08/08.cs
@@ -0,0 +1,19 @@
+﻿//  08
+using System;
+
+namespace CircleAreaandPerimeter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double r = double.Parse(Console.ReadLine());
+
+            double area = Math.PI*r*r;
+            double perimeter = 2*Math.PI*r;
+            
+            Console.WriteLine($"{area:F2}");
+            Console.WriteLine($"{perimeter:F2}");
+        }
+    }
+}
