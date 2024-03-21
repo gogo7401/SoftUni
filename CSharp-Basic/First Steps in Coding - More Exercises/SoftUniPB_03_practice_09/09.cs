e98e6e36d3c720e26d9a4df38fe7977e5a3e1ed9
Georgi Georgiev <joro74@gmail.com>
2024-03-21 09:09:31 +0200

    Add files via upload

---
+++ CSharp-Basic/First Steps in Coding - More Exercises/SoftUniPB_03_practice_09/09.cs
@@ -0,0 +1,16 @@
+﻿//  09
+using System;
+
+namespace WeatherForecast
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string weather = Console.ReadLine();
+
+            if (weather == "sunny") { Console.WriteLine("It's warm outside!"); }
+            else { Console.WriteLine("It's cold outside!"); }
+        }
+    }
+}
