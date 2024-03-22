1d34ef13adbcfc2efbdf601d2af0618a4451cb59
Georgi Georgiev <joro74@gmail.com>
2024-03-22 13:31:29 +0200

    Add files via upload

---
+++ CSharp-Basic/First Steps in Coding - More Exercises/SoftUniPB_03_practice_10/10.cs
@@ -0,0 +1,20 @@
+﻿//  10
+using System;
+
+namespace WeatherForecastPart2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double t = double.Parse(Console.ReadLine());
+
+            if ((t<5) || (t>35)) { Console.WriteLine("unknown"); }
+            if ((t >= 5) && (t <= 11.9)) { Console.WriteLine("Cold"); }
+            if ((t >= 12) && (t <= 14.9)) { Console.WriteLine("Cool"); }
+            if ((t >= 15) && (t <= 20)) { Console.WriteLine("Mild"); }
+            if ((t >= 20.1) && (t <= 25.9)) { Console.WriteLine("Warm"); }
+            if ((t >= 26) && (t <= 35)) { Console.WriteLine("Hot"); }
+        }
+    }
+}
