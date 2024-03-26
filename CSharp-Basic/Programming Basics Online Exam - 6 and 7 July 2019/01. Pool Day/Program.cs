c4ac61bfe98371257da99e713397d2c2212f8138
Georgi Georgiev <joro74@gmail.com>
2024-03-26 08:40:26 +0200

    Add files via upload

---
+++ CSharp-Basic/Programming Basics Online Exam - 6 and 7 July 2019/01. Pool Day/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace _01._Pool_Day
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int countPeople = int.Parse(Console.ReadLine());
+            double entryTax = double.Parse(Console.ReadLine());
+            double priceSunLounge = double.Parse(Console.ReadLine());
+            double priceSunUmbrella = double.Parse(Console.ReadLine());
+
+            double sum_entryTax = countPeople * entryTax;
+            double sum_priceSunLounge = Math.Ceiling(countPeople * 0.75)*priceSunLounge;
+            double sum_priceSunUmbrella = Math.Ceiling(countPeople * 0.5) * priceSunUmbrella;
+            double totalSum = sum_entryTax + sum_priceSunLounge + sum_priceSunUmbrella;
+
+            Console.WriteLine($"{totalSum:f2} lv.");
+        }
+    }
+}
