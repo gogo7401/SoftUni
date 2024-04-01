5ef5e40d59c923b9fd6185e5dad74c168f1b9ae6
Georgi Georgiev <joro74@gmail.com>
2024-04-01 10:29:04 +0300

    Add files via upload

---
+++ CSharp-Basic/Programming Basics Online Exam - 6 and 7 July 2019/05. PC Game Shop/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace _05._PC_Game_Shop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int numberOfSoldGames = int.Parse(Console.ReadLine());
+            string nameGame = "";
+            int countHearthstone = 0;
+            int countFornite = 0;
+            int countOverwatch = 0;
+            int countOthers = 0;
+
+            for (int i = 1; i <= numberOfSoldGames; i++)
+            {
+                nameGame = Console.ReadLine();
+                switch (nameGame)
+                {
+                    case "Hearthstone":
+                        countHearthstone++;
+                        break;
+                    case "Fornite":
+                        countFornite++;
+                        break;
+                    case "Overwatch":
+                        countOverwatch++;
+                        break;
+                    default:
+                        countOthers++;
+                        break;
+                }
+
+            }
+            int allSoldGames = countHearthstone + countFornite + countOverwatch + countOthers;
+            Console.WriteLine($"Hearthstone - {countHearthstone*1.0/ allSoldGames * 100:f2}%");
+            Console.WriteLine($"Fornite - {countFornite * 1.0 / allSoldGames * 100:f2}%");
+            Console.WriteLine($"Overwatch - {countOverwatch * 1.0 / allSoldGames * 100:f2}%");
+            Console.WriteLine($"Others - {countOthers * 1.0 / allSoldGames * 100:f2}%");
+
+        }
+    }
+}
