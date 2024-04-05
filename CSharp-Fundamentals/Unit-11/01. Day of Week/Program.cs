e42b9584135fd69e5afc224888723a68deeed84c
Georgi Georgiev <joro74@gmail.com>
2024-04-05 09:23:01 +0300

    Update Program.cs

--- CSharp-Fundamentals/Unit-11/01. Day of Week/Program.cs
+++ CSharp-Fundamentals/Unit-11/01. Day of Week/Program.cs
@@ -4,7 +4,7 @@ namespace _01._Day_of_Week
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void Main(string[] args) 
         {
             string[] daysOfWeek =
             {
